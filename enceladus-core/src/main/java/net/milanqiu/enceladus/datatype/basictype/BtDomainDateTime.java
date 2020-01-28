022a63d4bcec2686588465addc935ca093d21dd0
Milan Qiu <product@milanqiu.net>
2020-01-28 21:31:37 +0800

    Add BtDateTime and BtDomainDateTime

---
+++ enceladus-core/src/main/java/net/milanqiu/enceladus/datatype/basictype/BtDomainDateTime.java
@@ -0,0 +1,9 @@
+package net.milanqiu.enceladus.datatype.basictype;
+
+/**
+ * <p>
+ * Creation Date: 2017-04-17
+ * @author Milan Qiu
+ */
+public abstract class BtDomainDateTime extends BasicType {
+}
